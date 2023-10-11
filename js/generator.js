c8e7aa8c6fed25106a05e559b9959fd153f38462
Old Li's Mac <xinzhou.li9822@gmail.com>
2023-10-11 15:49:21 -0600

    Initial commit

---
+++ js/generator.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const round = 1;
+const currentPlayer = 0;
+const 
+window.onload = function (){
+    document.getElementById("round_num").innerHTML = round;
+};
+
+
+function drawboard(){
+
+}
+
+function temp (){
+    console.log("wh")
+};
+
