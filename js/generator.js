65fcdb890441cf4eb4c28ac84e9aa56a0bfe6b5c
XinzhouLi <48045064+XinzhouLi@users.noreply.github.com>
2023-10-11 23:16:43 -0600

    basic pattern of chessgrid is done

--- js/generator.js
+++ js/generator.js
@@ -2,16 +2,44 @@
 
 const round = 1;
 const currentPlayer = round%2;
+const chessBoard = []
+const chessRecord = []
+
+// init the
 window.onload = function (){
     document.getElementById("round_num").innerHTML = round;
 };
 
 
-function drawboard(){
+async function drawBoard(){
+    const boardRoot = document.getElementById("checkerboard")
+    let tempNode = await emptyPiece()
+    // tempNode.innerHTML = 'hello'
+    boardRoot.appendChild(tempNode)
+}
+
+
+async function emptyPiece(){
+    let boardGrid = document.createElement("div")
+    boardGrid.className = "chessGrid"
+    let h = document.createElement("div")
+    h.className = "h"
+    let v = document.createElement("div")
+    v.className = "v"
+    let piece = document.createElement("button")
+    piece.className = "white"
+    boardGrid.appendChild(h)
+    boardGrid.appendChild(v)
+    boardGrid.appendChild(piece)
+    return boardGrid
+}
+
+function placedPiece(){
 
 }
 
 function temp (){
     console.log("wh")
+    drawBoard()
 }
 
